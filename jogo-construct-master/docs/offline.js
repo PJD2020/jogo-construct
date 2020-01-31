9821b9230d005e802d202009a94cc49dc537ade0
allancfb <44479729+allancfb@users.noreply.github.com>
2020-01-31 12:03:54 -0300

    Sistema de danos

---
+++ jogo-construct-master/docs/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1580445926,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/block-sheet0.png",
+		"images/player-sheet0.png",
+		"images/sword-sheet0.png",
+		"images/spawngoblin-sheet0.png",
+		"images/goblin-sheet0.png",
+		"images/background-sheet0.png",
+		"images/newgamebutton.png",
+		"images/newgamebackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
